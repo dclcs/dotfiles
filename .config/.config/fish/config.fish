a3d769b9e11cdee3ec79224aff13bc3d52f48576
Canglan Dai <canglandai@CanglandeMacBook-Pro.local>
2026-03-15 22:24:11 +0800

    add: ghostty

--- .config/.config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/daicanglan/anaconda3/bin/conda
-    eval /Users/daicanglan/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
